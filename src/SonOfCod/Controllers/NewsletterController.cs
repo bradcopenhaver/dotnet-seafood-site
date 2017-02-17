526f10a08a373336b32afabd33521c06e2dadae4
Brad <bradcopenhaver@gmail.com>
2017-02-17 13:16:41 -0800

    move newsletter functions to NewsletterController

--- src/SonOfCod/Controllers/NewsletterController.cs
+++ src/SonOfCod/Controllers/NewsletterController.cs
@@ -3,51 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SonOfCod.Models;
 using Microsoft.AspNetCore.Identity;
+using SonOfCod.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SonOfCod.Controllers
 {
-    public class HomeController : Controller
+    public class NewsletterController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ApplicationDbContext _db;
-        
-        public HomeController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+
+        public NewsletterController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
         {
             _userManager = userManager;
             _db = db;
         }
-        // GET: /<controller>/
+        //GET: /Newsletter/Index/
         public IActionResult Index()
         {
             return View();
         }
-        //GET: /Home/Subscribe/
+        //GET: /Newsletter/Subscribe/
         public IActionResult Subscribe()
         {
             return View();
         }
-        //POST: /Home/Subscribe/
+        //POST: /Newsletter/Subscribe/
         [HttpPost]
         public IActionResult Subscribe(Subscriber subscriber)
         {
             _db.Subscribers.Add(subscriber);
             _db.SaveChanges();
-            return RedirectToAction("SubSuccess" , new { id = subscriber.SubscriberId });
+            return RedirectToAction("SubSuccess", new { id = subscriber.SubscriberId });
         }
-        //GET: /Home/Unsubscribe/
+        //GET: /Newsletter/Unsubscribe/
         public IActionResult Unsubscribe()
         {
             return View();
         }
-        //POST: /Home/Unsubscribe/
+        //POST: /Newsletter/Unsubscribe/
         [HttpPost]
         public IActionResult Unsubscribe(Subscriber subscriber)
         {
-            Subscriber currentSub = _db.Subscribers.FirstOrDefault(s=> s.Email == subscriber.Email);
+            Subscriber currentSub = _db.Subscribers.FirstOrDefault(s => s.Email == subscriber.Email);
             if (currentSub != null)
             {
                 _db.Remove(currentSub);
@@ -60,14 +60,14 @@ namespace SonOfCod.Controllers
             }
         }
 
-        //GET: /Home/SubSuccess/
+        //GET: /Newsletter/SubSuccess/
         public IActionResult SubSuccess(int id)
         {
             ViewBag.Subscriber = _db.Subscribers.FirstOrDefault(s => s.SubscriberId == id);
             return View();
         }
 
-        //GET: /Home/UnsubSuccess/
+        //GET: /Newsletter/UnsubSuccess/
         public IActionResult UnsubSuccess()
         {
             return View();
